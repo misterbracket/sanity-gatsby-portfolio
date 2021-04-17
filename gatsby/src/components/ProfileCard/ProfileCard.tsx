cd6aad62a0ca924331a1549ea1014554cbb3f3df
MaxKlammer <maximilian.klammer@betssongroup.com>
2021-04-17 10:45:30 +0200

    profile card fix

--- gatsby/src/components/ProfileCard/ProfileCard.tsx
+++ gatsby/src/components/ProfileCard/ProfileCard.tsx
@@ -17,6 +17,7 @@ const ProfileCardStyles = styled.div`
   margin: 3rem auto;
   background: var(--light-pink);
   box-shadow: var(--shd, 0 1px 4px rgba(0, 0, 0, 0.6));
+  width: 370px;
   max-width: 370px;
   align-self: center;
   @media screen and (min-width: 840px) {
