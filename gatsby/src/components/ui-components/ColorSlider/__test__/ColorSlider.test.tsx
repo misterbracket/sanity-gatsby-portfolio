2c75bb6b2d7ce811060fc7ad0dcdc8baaad5f56f
MaxKlammer <maximilian.klammer@betssongroup.com>
2021-06-03 07:46:23 +0200

    refactored tests

--- gatsby/src/components/ui-components/ColorSlider/__test__/ColorSlider.test.tsx
+++ gatsby/src/components/ui-components/ColorSlider/__test__/ColorSlider.test.tsx
@@ -4,47 +4,46 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import { QUERIES } from "../../../../styles/constants";
 
-beforeEach(() => {
-  renderComponent();
-});
-const renderComponent = () =>
-  render(
+const renderComponent = () => {
+  const utils = render(
     <ThemeProvider theme={{ queries: QUERIES }}>
       <ColorSlider />
     </ThemeProvider>
   );
+  const sliderOne = screen.getByTestId("slider-one") as HTMLInputElement;
+  const sliderTwo = screen.getByTestId("slider-two") as HTMLInputElement;
 
-describe("Color Slider", () => {
-  it("Slider Two starts with the right color one", () => {
-    const slider = screen.getByTestId("slider-one") as HTMLInputElement;
-    expect(slider.value).toBe("30");
+  return { ...utils, sliderOne, sliderTwo };
+};
+
+test("Slider Two starts with the right color one", () => {
+  const { sliderOne } = renderComponent();
+  expect(sliderOne.value).toBe("30");
+});
+
+test("Slider Two starts with the right color two", () => {
+  const { sliderTwo } = renderComponent();
+  expect(sliderTwo).toHaveValue("221");
+});
+
+test("Change the Slider One Value", () => {
+  const { sliderOne } = renderComponent();
+
+  fireEvent.change(sliderOne, {
+    target: {
+      value: "60",
+    },
   });
+  expect(sliderOne).toHaveValue("60");
+});
 
-  it("Slider One starts with the right color two", () => {
-    const slider = screen.getByTestId("slider-two") as HTMLInputElement;
+test("Change the Slider Two Value", () => {
+  const { sliderTwo } = renderComponent();
 
-    expect(slider).toHaveValue("221");
+  fireEvent.change(sliderTwo, {
+    target: {
+      value: "60",
+    },
   });
-
-  it("Change the Slider One Value", () => {
-    const slider = screen.getByTestId("slider-one") as HTMLInputElement;
-
-    fireEvent.change(slider, {
-      target: {
-        value: "60",
-      },
-    });
-    expect(slider).toHaveValue("60");
-  });
-
-  it("Change the Slider One Value", () => {
-    const slider = screen.getByTestId("slider-two") as HTMLInputElement;
-
-    fireEvent.change(slider, {
-      target: {
-        value: "60",
-      },
-    });
-    expect(slider).toHaveValue("60");
-  });
+  expect(sliderTwo).toHaveValue("60");
 });
