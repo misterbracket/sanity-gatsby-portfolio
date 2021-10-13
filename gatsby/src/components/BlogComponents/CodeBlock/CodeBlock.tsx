e7564f5014e870f2ae6d5c0aa406e994487635e2
Max Klammer <maximilian.klammer@gmail.com>
2021-10-13 08:15:46 +0200

    fix codeblock size on small devices

--- gatsby/src/components/BlogComponents/CodeBlock/CodeBlock.tsx
+++ gatsby/src/components/BlogComponents/CodeBlock/CodeBlock.tsx
@@ -1,15 +1,18 @@
 import Highlight, { defaultProps, Language } from 'prism-react-renderer';
 import nightOwl from 'prism-react-renderer/themes/nightOwl';
 import React from 'react';
+import styled from 'styled-components';
 
-
+const Pre = styled.pre`
+  white-space: pre-wrap;
+`;
 export default ({ children, className }: {children: string, className: `language-${Language}`}) => {
   const language = className.replace(/language-/, '') as Language
 
   return (
       <Highlight  {...defaultProps} theme={nightOwl} code={children} language={language}>
       {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={className} style={{...style, padding: '20px'}}>
+        <Pre className={className} style={{...style, padding: '20px'}}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({line, key: i})}>
               {line.map((token, key) => (
@@ -17,7 +20,7 @@ export default ({ children, className }: {children: string, className: `language
               ))}
             </div>
           ))}
-        </pre>
+        </Pre>
       )}
     </Highlight>
   )
